f3c8857f997e7b3d43012065efa24cb2ace3045a
LDS <luanddt.dev@gmail.com>
2023-12-21 15:58:04 +0700

    update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
+
+import { motion } from "framer-motion"
+import { navVariants } from "@/utils"
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <motion.nav
+            variants={navVariants}
+            initial="hidden"
+            whileInView="show"
+            className="padding-x py-8 relative"
+        >
+            <div className="absolute w-1/2 inset-0 navbar-gradient" />
+
+            <div className="max-width flex justify-between">
+                <img 
+                    src="/assets/icons/search.svg"
+                    alt="search"
+                    className="w-6 h-6 object-contain"
+                />
+
+                <h2 className="font-extrabold text-[24px] leading-[30px] text-white uppercase">
+                    METAVERSUS
+                </h2>
+
+                <img 
+                    src="/assets/icons/menu.svg"
+                    alt="menu"
+                    className="w-6 h-6 object-contain"
+                />
+            </div>
+        </motion.nav>
     )
 }
 
