337da9c9b855f77139d3eb5674b4640e1ed98fa9
LDS <luanddt.dev@gmail.com>
2023-12-23 12:51:20 +0700

    update home

--- app/page.js
+++ app/page.js
@@ -6,13 +6,22 @@ export default function Home() {
     <div className="bg-primary-black overflow-hidden">
       <Navbar />
       <Hero />
-      <About />
-      <Explore />
-      <GetStarted />
-      <WhatsNew />
+      <div className="relative">
+        <About />
+        <div className="gradient-01 z-0" />
+        <Explore />
+      </div>
+      <div className="relative">
+        <GetStarted />
+        <div className="gradient-02 z-0" />
+        <WhatsNew />
+      </div>
       <World />
-      <Insights />
-      <Feedback />
+      <div className="relative">
+        <Insights />
+        <div className="gradient-02 z-0" />
+        <Feedback />
+      </div>
       <Footer />
     </div>
   )
